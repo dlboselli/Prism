98330b83f5c1a497fda6bac8ef39b7dc3f703c79
David Boselli <dlboselli@gmail.com>
2025-11-22 04:05:44 -0800

    Base Template

---
+++ Prism/PrismApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrismApp.swift
+//  Prism
+//
+//  Created by David Boselli on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PrismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
